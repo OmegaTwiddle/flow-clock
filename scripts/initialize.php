915daf46ae5fb41ee97d23303722e02ec8ef0d74
Alex Wormuth <awormuth@usc.edu>
2014-01-16 23:03:18 -0800

    some php stuff

--- scripts/initialize.php
+++ scripts/initialize.php
@@ -2,7 +2,7 @@
 
 	session_start();
 
-	$fileContent = file('dbInfo.txt', FILE_IGNORE_NEW_LINES);
+	$fileContent = readfile('dbInfo.txt');
 
 	//check if username exists
 	$dbconnection = mysql_connect("localhost", $fileContent[0], $fileContent[1]);
@@ -14,7 +14,7 @@
 	if ($_SESSION['username']){
 		echo json_encode($_SESSION['username'] . " is logged in!");
 	} else {
-		echo json_encode($fileContent[0] . " " . $fileContent[1]);
+		echo json_encode($fileContent . " " . $fileContent[0]);
 	}
 
 ?>
