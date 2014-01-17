b939fd1078a2f6e2e3cac137a9f9568e35f5883f
Alex Wormuth <awormuth@usc.edu>
2014-01-17 00:48:05 -0800

    some php stuff

--- scripts/createClock.php
+++ scripts/createClock.php
@@ -4,6 +4,18 @@
 
 	list($user, $pass, $extra) = explode(",", file_get_contents('/home/flow-clock/conf/db_conf'));
 
+	//check if username exists
+	$dbconnection = mysql_connect("localhost", (string)$user, (string)$pass);
+	mysql_select_db("flow-clock", $dbconnection);
+
+	$sql = "INSERT INTO clocks
+			(name)
+			VALUES
+			('" . $_GET["clockName"] . "')";
+
+	mysql_query($sql);
+
+
 	
 
 	echo "success";
