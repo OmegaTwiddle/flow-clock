b91f857ceb40ddbd532e71f23118cfe7c1225862
Alex Wormuth <awormuth@usc.edu>
2014-01-17 00:45:30 -0800

    some php stuff

--- scripts/createClock.php
+++ scripts/createClock.php
@@ -4,10 +4,6 @@
 
 	list($user, $pass, $extra) = explode(",", file_get_contents('/home/flow-clock/conf/db_conf'));
 
-	//check if username exists
-	$dbconnection = mysql_connect("localhost", (string)$user, (string)$pass);
-	mysql_select_db("flow-clock", $dbconnection);
-
 	
 
 	echo json_encode("success");
