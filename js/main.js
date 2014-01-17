07a97ac5097fcef977cf62c64c548b7edb3c488a
Alex Wormuth <awormuth@usc.edu>
2014-01-17 00:19:27 -0800

    some php stuff

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
         type: 'GET',
         url: 'scripts/initialize.php',
         success: function(msg){
-            console.log(msg);
-            $(document.body).prepend(msg.username + " is logged in!");
-            $(document.body).append("<div hidden id='hiddenUserID'>" + msg.userID + "</div>");
+            var message = msg;
+            $(document.body).prepend(message.username + " is logged in!");
+            $(document.body).append("<div hidden id='hiddenUserID'>" + message.userID + "</div>");
         } 
     });
 
